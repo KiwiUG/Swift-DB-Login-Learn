c04d64ab636d0ff859b8c02484ecf76b95291bae
KiwiUG <kiwiug69@gmail.com>
2025-05-25 13:17:50 +0545

    Initial Commit

---
+++ Learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learning
+//
+//  Created by Utsav Gautam on 25/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Utsav Gautam on 25/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
